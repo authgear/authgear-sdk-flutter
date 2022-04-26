0411cf34cb9d3936519be979c3fec61b29df01e1
Louis Chan <louischan@oursky.com>
2022-04-26 16:44:19 +0800

    Break _MyAppState into many stateless widgets

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:async' show StreamSubscription;
 
 import 'package:flutter_authgear/authgear.dart';
 
 void main() {
+  // debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
@@ -15,6 +17,103 @@ class MyApp extends StatefulWidget {
 }
 
 const redirectURI = "com.authgear.exampleapp.flutter://host/path";
+
+class AuthenticateButton extends StatelessWidget {
+  final Authgear authgear;
+  final void Function(dynamic) onError;
+
+  const AuthenticateButton(
+      {Key? key, required this.authgear, required this.onError})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: authgear.sessionState == SessionState.noSession
+          ? _onPressAuthenticate
+          : null,
+      child: const Text("Authenticate"),
+    );
+  }
+
+  Future<void> _onPressAuthenticate() async {
+    try {
+      await authgear.authenticate(redirectURI: redirectURI);
+    } catch (e) {
+      onError(e);
+    }
+  }
+}
+
+class GetUserInfoButton extends StatelessWidget {
+  final Authgear authgear;
+  final void Function(dynamic) onError;
+
+  const GetUserInfoButton(
+      {Key? key, required this.authgear, required this.onError})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: authgear.sessionState == SessionState.authenticated
+          ? () {
+              _onPressGetUserInfo(context);
+            }
+          : null,
+      child: const Text("Get UserInfo"),
+    );
+  }
+
+  Future<void> _onPressGetUserInfo(BuildContext context) async {
+    try {
+      final userInfo = await authgear.getUserInfo();
+      await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("User Info"),
+            content: Text(
+              "sub: ${userInfo.sub}\nisAnonymous: ${userInfo.isAnonymous}\nisVerified: ${userInfo.isVerified}",
+            ),
+            actions: [
+              TextButton(
+                child: const Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      onError(e);
+    }
+  }
+}
+
+class LogoutButton extends StatelessWidget {
+  final Authgear authgear;
+  final void Function(dynamic) onError;
+  const LogoutButton({Key? key, required this.authgear, required this.onError})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: authgear.sessionState == SessionState.authenticated
+          ? _onPressLogout
+          : null,
+      child: const Text("Logout"),
+    );
+  }
+
+  Future<void> _onPressLogout() async {
+    await authgear.logout();
+  }
+}
 
 class _MyAppState extends State<MyApp> {
   final Authgear _authgear =
@@ -54,71 +153,16 @@ class _MyAppState extends State<MyApp> {
             ),
             body: Center(
                 child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                TextButton(
-                  onPressed: _authgear.sessionState == SessionState.noSession
-                      ? _onPressAuthenticate
-                      : null,
-                  child: const Text("Authenticate"),
-                ),
-                TextButton(
-                  onPressed:
-                      _authgear.sessionState == SessionState.authenticated
-                          ? () {
-                              _onPressGetUserInfo(context);
-                            }
-                          : null,
-                  child: const Text("Get UserInfo"),
-                ),
-                TextButton(
-                  onPressed:
-                      _authgear.sessionState == SessionState.authenticated
-                          ? _onPressLogout
-                          : null,
-                  child: const Text("Logout"),
-                ),
+                AuthenticateButton(authgear: _authgear, onError: (_e) {}),
+                GetUserInfoButton(authgear: _authgear, onError: (_e) {}),
+                LogoutButton(authgear: _authgear, onError: (_e) {}),
               ],
             )),
           );
         },
       ),
-    );
-  }
-
-  Future<void> _onPressAuthenticate() async {
-    try {
-      final result = await _authgear.authenticate(redirectURI: redirectURI);
-      print("result: ${result.userInfo.sub}");
-    } catch (e) {
-      print("error: $e");
-    }
-  }
-
-  Future<void> _onPressLogout() async {
-    await _authgear.logout();
-  }
-
-  Future<void> _onPressGetUserInfo(BuildContext context) async {
-    final userInfo = await _authgear.getUserInfo();
-    await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("User Info"),
-          content: Text(
-            "sub: ${userInfo.sub}\nisAnonymous: ${userInfo.isAnonymous}\nisVerified: ${userInfo.isVerified}",
-          ),
-          actions: [
-            TextButton(
-              child: const Text("OK"),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
     );
   }
 }

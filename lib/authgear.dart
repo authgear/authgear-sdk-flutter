3b35d136ca5bada0b57630d5d20d9bb26d02d9a9
Louis Chan <louischan@oursky.com>
2022-05-04 18:05:07 +0800

    Publish docs with gh-pages

--- lib/authgear.dart
+++ lib/authgear.dart
@@ -1,3 +1,20 @@
+/// Authgear Flutter SDK.
+///
+/// To get started, create an instance of [Authgear].
+/// You should use a single instance throughout the app.
+///
+/// The session state could change at any given time,
+/// subscribe to [Authgear.onSessionStateChange] to listen for any state change event.
+///
+/// Use [Authgear.authenticate] to authenticate the end user.
+///
+/// Use [Authgear.open] to open the settings page to let the end-user to manage their account settings.
+///
+/// Use [Authgear.wrapHttpClient] to make a [Client] capable of injecting access token into Authorization header.
+library authgear;
+
+import 'package:http/http.dart' show Client;
+
 export 'src/type.dart'
     show
         SessionState,

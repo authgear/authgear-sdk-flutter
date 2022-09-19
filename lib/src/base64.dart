0ad5aafa0cee4d4cd757646b64a8ffb00d851c8c
OuJiaHao <30933166+lonyehan@users.noreply.github.com>
2022-09-20 02:35:17 +0800

    Update base64.dart
    I think use switch case will improve code readability.

--- lib/src/base64.dart
+++ lib/src/base64.dart
@@ -7,13 +7,18 @@ String base64UrlEncode(List<int> bytes) {
 
 List<int> base64UrlDecode(String s) {
   final r = s.length % 4;
-  final padding = r == 1
-      ? "==="
-      : r == 2
-          ? "=="
-          : r == 3
-              ? "="
-              : "";
+  final padding;
+  
+  switch(r){
+    case 1:
+    case 2:
+    case 3:
+      padding = "=" * (4-r);
+      break;
+    default:
+      padding = "";
+  }
+  
   final padded = s + padding;
   return base64Url.decode(padded);
 }

62c740199176f3c7a265bd043ba36239c6825e85
Tung Wu <tung@oursky.com>
2025-02-04 17:30:22 +0800

    Remove unused imports

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async' show StreamSubscription;
 import 'dart:io';
-import 'dart:developer' as developer;
 import 'dart:convert';
 import 'package:flutter/material.dart' hide ColorScheme;
 import 'package:flutter/services.dart';

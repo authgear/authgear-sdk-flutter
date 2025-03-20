39e2cfe88155fdfbdc78228225da52a93b228f46
Louis Chan <louischan@oursky.com>
2025-03-20 19:56:59 +0800

    Do not shadow native toolchain in flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -19,18 +15,34 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
+      # This discussion inspired me
+      # https://discourse.nixos.org/t/best-practices-for-expo-react-native-development-with-devenv/58776/5
+      #
+      # What we want to do here is just provision the listed packages below,
+      # without the clang compiler nor Apple SDK.
+      # So we need to undo some side effects of mkShellNoCC.
       {
-        devShells.default = pkgs.mkShell {
+        # Use mkShellNoCC instead of mkShell so that it wont pull in clang.
+        # We need to use the clang from Xcode.
+        devShells.default = pkgs.mkShellNoCC {
           packages = [
             # 20.18.1
             pkgs.nodejs_20
             # 3.3.6
             pkgs.ruby_3_3
-            # 1.16.2
-            pkgs.cocoapods
             # 3.27.1
             pkgs.flutter327
           ];
+          # Even we use mkShellNoCC, DEVELOPER_DIR, SDKROOT, MACOSX_DEPLOYMENT_TARGET is still set.
+          # We undo that.
+          #
+          # Also, xcrun from Nix is put in PATH, we want to undo that as well.
+          shellHook = ''
+            export PATH=$(echo $PATH | sed "s,${pkgs.xcbuild.xcrun}/bin,,")
+            unset DEVELOPER_DIR
+            unset SDKROOT
+            unset MACOSX_DEPLOYMENT_TARGET
+          '';
         };
       }
     );
